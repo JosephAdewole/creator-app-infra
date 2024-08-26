f73ec29ce9fdec2ff23fafc1a12d7f528d9665c3
Joseph Adewole <Josephadewole1@gmail.com>
2024-08-26 17:42:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "example" {
   ami			                = "ami-785db401"
   instance_type           = "t2.micro"
   vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
-  monitoring = false
+  monitoring = true
   
   user_data = <<-EOF
 	      #!/bin/bash
